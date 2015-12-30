1a97914735b88ced3762c7b0813ac87030730226
Erik Thorselius <erik.thorselius@gmail.com>
2015-12-30 08:17:45 +0100

    Follow python indentetion

--- watt_sender.py
+++ watt_sender.py
@@ -1,14 +1,17 @@
-import ssl, json, zmq, os, logging, sys, sched, time, signal 
-from urllib.request import Request, urlopen 
+import ssl, json, zmq, os, logging, sys, sched, time, signal
+from urllib.request import Request, urlopen
+
 
 def signal_handler(signal, frame):
-  print('You pressed Ctrl+C! Shutting down')
-  socket.close(linger=1)
-  sys.exit(0)
+    print('You pressed Ctrl+C! Shutting down')
+    socket.close(linger=1)
+    sys.exit(0)
+
+
 signal.signal(signal.SIGTERM, signal_handler)
 signal.signal(signal.SIGINT, signal_handler)
 
-logging.basicConfig(format='%(asctime)s %(message)s',level=logging.DEBUG)
+logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG)
 report_time = float(os.getenv('WATT_REPORT_TIME', 3600))
 socket_address = os.getenv('WATT_SOCKET').strip()
 api_url = os.getenv('WATT_API_URL').strip()
@@ -16,30 +19,34 @@ api_token = os.getenv('WATT_API_TOKEN').strip()
 
 context = zmq.Context()
 socket = context.socket(zmq.PUSH)
-logging.info('Connected to address %s',socket_address)
+logging.info('Connected to address %s', socket_address)
 socket.connect(socket_address)
 
+
 def send(message):
-  logging.debug('Send value: %s to server',message)
-  socket.send_json(message)
+    logging.debug('Send value: %s to server', message)
+    socket.send_json(message)
+
 
 def get_data_from_api():
-  q = Request(api_url)
-  q.add_header('Accept', 'application/json')
-  q.add_header('X-Version', '1.0')
-  q.add_header('X-Token', api_token)
-  response = urlopen(q, context=ssl._create_unverified_context()).read()
-  return json.loads(response.decode('utf-8'))
+    q = Request(api_url)
+    q.add_header('Accept', 'application/json')
+    q.add_header('X-Version', '1.0')
+    q.add_header('X-Token', api_token)
+    response = urlopen(q, context=ssl._create_unverified_context()).read()
+    return json.loads(response.decode('utf-8'))
+
 
 def report(sc):
-  filterd_elements = [element for element in get_data_from_api() if element[1] !='nan']
-  messages = [{"name":"Electrical sensor",
-      "type":"external_api", 
-      "sensor":"electricity",
-      "unix_timestamp":element[0], 
-      "value":element[1]} for element in filterd_elements]
-  [send(message) for message in messages]
-  sc.enter(report_time, 1, report, (sc,))
+    filterd_elements = [element for element in get_data_from_api() if element[1] != 'nan']
+    messages = [{"name": "Electrical sensor",
+                 "type": "external_api",
+                 "sensor": "electricity",
+                 "unix_timestamp": element[0],
+                 "value": element[1]} for element in filterd_elements]
+    [send(message) for message in messages]
+    sc.enter(report_time, 1, report, (sc,))
+
 
 s = sched.scheduler(time.time, time.sleep)
 s.enter(report_time, 1, report, (s,))
